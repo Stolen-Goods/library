896f3df83345a2ca33ce9719047c3d75545105e3
Matt <hurley04155@gmail.com>
2024-10-21 20:16:55 -0400

    Create add button and remove button

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
+let index = 0;
+const addNewBook = document.getElementById("dialog-box");
 
-function Book(title, author, pages, haveRead) {
+function CreateBook(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
 }
 
-
-
-const mistborn = new Book("Mistborn", "Brandon Sanderson", 680, true);
-const dune = new Book("Dune", "Frank Herbert", 820, true);
-const hobbit = new Book("The Hobbit", "JRR Tolkien", 300, true)
+const mistborn = new CreateBook("Mistborn", "Brandon Sanderson", 680, true);
+const dune = new CreateBook("Dune", "Frank Herbert", 820, true);
+const hobbit = new CreateBook("The Hobbit", "JRR Tolkien", 300, true)
 
 const library = [mistborn, dune, hobbit];
 
-const button = document.getElementById("create-button")
-button.addEventListener("click", addBookToLibrary);
+const addButton = document.getElementById("create-button")
+addButton.addEventListener("click", () => {
+    addNewBook.showModal();
+});
 
-const newBook = document.getElementById("dialog-box");
-
-function addBookToLibrary() {
-    newBook.showModal();
-
-}
-
-function display() {
-    let index = 0;
+function libraryDisplay() {
     for (let i = 0; i < library.length; i++) {
         const shelf = library[index];
         const book = document.createElement("div");
         book.innerText = `${shelf.title}\n by ${shelf.author}\n ${shelf.pages} pages`;
+        const removeButton = document.createElement("button");
+        removeButton.type = "Button";
+        removeButton.innerText = "Remove";
+        removeButton.addEventListener("click", () => {
+            book.remove();
+        })
+        book.appendChild(removeButton);
         document.body.appendChild(book);
         index++;
     }
 }
 
-
-
-
-
-display();
-
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    let newBook = new CreateBook(title, author, pages);
+    library.push(newBook);
+    addNewBook.close();
+    libraryDisplay();
+})
